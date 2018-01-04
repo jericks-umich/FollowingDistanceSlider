5da2e84094aeddd1a63ed5d7f6c91a53d4c89d36
Jeremy Erickson <jeremy@jeremy-erickson.com>
2018-01-04 15:59:39 -0500

    cleaned some things up, others not cleaned up

--- followingdistance.py
+++ followingdistance.py
@@ -5,18 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 
-# all units are metric
-v1_initial_velocity = 0 # m/s
-v2_initial_velocity = 0 # m/s
-initial_velocity_range = np.arange(20,35.1,.1) # approximately 45-78 mph (72-126 km/h)
-v1_decel = 0 # m/s^2
-v2_decel = 0 # m/s^2
-decel_range = np.arange(-6,-11.01,-.01)
-decel_delay = 0 # seconds
-decel_delay_range = np.arange(0,1.001,.001)
-# calculated variable - how much farther v2 travels than v1
-distance_threshold = 0 # meters 
-
 # we wish to calculate the difference in stopping distance between v1 (leader)
 # and v2 (follower), given their initial velocities, deceleration rates, and
 # the delay between the leader starting to decelerate and the follower doing
@@ -32,6 +20,9 @@ distance_threshold = 0 # meters
 # stopping distance for v2 is the same, but also includes the distance traveled
 # during the delay (b), before deceleration starts
 # d = v*b - 1/2(v^2/a)
+
+def distance(v1_vel, v2_vel, v1_dec, v2_dec, delay):
+  return (v2_vel*delay - (1.0/2.0*(v2_vel**2/v2_dec))) - (-1.0/2.0*(v1_vel**2/v1_dec))
 
 # start some test plots, assuming starting values as follows:
 # initial velocities: 29m/s
@@ -53,6 +44,18 @@ v1_decel = v1_initial_decel
 v2_decel = v2_initial_decel
 decel_delay = initial_decel_delay
 
+def distance_for_v1_vel():
+  return [distance(v1_temp_velocity, v2_velocity, v1_decel, v2_decel, decel_delay) for v1_temp_velocity in velocity_range]
+def distance_for_v2_vel():
+  return [distance(v1_velocity, v2_temp_velocity, v1_decel, v2_decel, decel_delay) for v2_temp_velocity in velocity_range]
+def distance_for_v1_dec():
+  return [distance(v1_velocity, v2_velocity, v1_temp_decel, v2_decel, decel_delay) for v1_temp_decel in decel_range]
+def distance_for_v2_dec():
+  return [distance(v1_velocity, v2_velocity, v1_decel, v2_temp_decel, decel_delay) for v2_temp_decel in decel_range]
+def distance_for_delay():
+  return [distance(v1_velocity, v2_velocity, v1_decel, v2_decel, decel_temp_delay) for decel_temp_delay in delay_range]
+
+
 # plots will look like the following:
 # /-----------------\
 # | v1_vel | v2_vel |
@@ -68,7 +71,7 @@ axcolor = 'lightgoldenrodyellow'
 
 # v1 initial velocity varies
 x = velocity_range
-y = [(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_temp_velocity**2/v1_decel)) for v1_temp_velocity in velocity_range]
+y = distance_for_v1_vel()
 plt.subplot(3,2,1)
 v1_vel_plot, = plt.plot(x,y)
 plt.ylabel("distance (m)")
@@ -76,7 +79,7 @@ plt.xlabel("v1 initial velocity (m/s)")
 
 # v2 initial velocity varies
 x = velocity_range
-y = [(v2_temp_velocity*decel_delay - (1.0/2.0*(v2_temp_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for v2_temp_velocity in velocity_range]
+y = distance_for_v2_vel()
 plt.subplot(3,2,2)
 v2_vel_plot, = plt.plot(x,y)
 plt.ylabel("distance (m)")
@@ -84,7 +87,7 @@ plt.xlabel("v2 initial velocity (m/s)")
 
 # v1 deceleration varies
 x = decel_range
-y = [(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_temp_decel)) for v1_temp_decel in decel_range]
+y = distance_for_v1_dec()
 plt.subplot(3,2,3)
 v1_dec_plot, = plt.plot(x,y)
 plt.ylabel("distance")
@@ -92,7 +95,7 @@ plt.xlabel("v1 deceleration (m/s^2)")
 
 # v2 deceleration varies
 x = decel_range
-y = [(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_temp_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for v2_temp_decel in decel_range]
+y = distance_for_v2_dec()
 plt.subplot(3,2,4)
 v2_dec_plot, = plt.plot(x,y)
 plt.ylabel("distance")
@@ -100,7 +103,8 @@ plt.xlabel("v2 deceleration (m/s^2)")
 
 # deceleration delay varies
 x = delay_range
-y = [(v2_velocity*decel_temp_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for decel_temp_delay in delay_range]
+y = distance_for_delay()
+plt.subplot(3,2,4)
 plt.subplot(3,1,3)
 delay_plot, = plt.plot(x,y)
 plt.ylabel("distance")
@@ -119,16 +123,21 @@ axdelay = plt.axes([.25, 0.05,  0.5, 0.03], facecolor=axcolor)
 s_delay = Slider(axdelay, "Delay", 0, 1, valinit=initial_decel_delay)
 
 def update(val):
+  global v1_velocity
+  global v2_velocity
+  global v1_decel
+  global v2_decel
+  global decel_delay 
   v1_velocity = s_v1vel.val
   v2_velocity = s_v2vel.val
   v1_decel    = s_v1dec.val
   v2_decel    = s_v2dec.val
   decel_delay = s_delay.val
-  v1_vel_plot.set_ydata([(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_temp_velocity**2/v1_decel)) for v1_temp_velocity in velocity_range])
-  v2_vel_plot.set_ydata([(v2_temp_velocity*decel_delay - (1.0/2.0*(v2_temp_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for v2_temp_velocity in velocity_range])
-  v1_dec_plot.set_ydata([(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_temp_decel)) for v1_temp_decel in decel_range])
-  v2_dec_plot.set_ydata([(v2_velocity*decel_delay - (1.0/2.0*(v2_velocity**2/v2_temp_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for v2_temp_decel in decel_range])
-  delay_plot.set_ydata([(v2_velocity*decel_temp_delay - (1.0/2.0*(v2_velocity**2/v2_decel))) - (-1.0/2.0*(v1_velocity**2/v1_decel)) for decel_temp_delay in delay_range])
+  v1_vel_plot.set_ydata(distance_for_v1_vel())
+  v2_vel_plot.set_ydata(distance_for_v2_vel())
+  v1_dec_plot.set_ydata(distance_for_v1_dec())
+  v2_dec_plot.set_ydata(distance_for_v2_dec())
+  delay_plot.set_ydata(distance_for_delay())
 s_v1vel.on_changed(update)
 s_v2vel.on_changed(update)
 s_v1dec.on_changed(update)
